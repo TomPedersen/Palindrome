2772aeb1e3869bdacb5b94fb76e6affb35bd598d
TomPedersen <Knives89@gmail.com>
2015-10-15 20:21:34 +1300

    few unit tests written, assuming method will return a bool for now

--- PalindromeChecker/UnitTest1.cs
+++ PalindromeChecker/UnitTest1.cs
@@ -7,8 +7,24 @@ namespace PalindromeChecker
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IsPalindrome_True()
         {
+            Palindrome obj = new Palindrome();
+            Assert.IsTrue(obj.CheckIfPalindrome("racecar"));
+        }
+
+        [TestMethod]
+        public void IsPalindrome_True2()
+        {
+            Palindrome obj = new Palindrome();
+            Assert.IsTrue(obj.CheckIfPalindrome("Swap God for a janitor; rot in a jar of dog paws."));
+        }
+
+        [TestMethod]
+        public void IsPalindrome_False()
+        {
+            Palindrome obj = new Palindrome();
+            Assert.IsFalse(obj.CheckIfPalindrome("This is not a palindrome"));
         }
     }
 }
