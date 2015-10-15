f95b4d56a367c968d0e8f41470e90260d6adfc82
TomPedersen <Knives89@gmail.com>
2015-10-15 20:33:54 +1300

    using regex to strip whitespace and punctuation from all strings entered into method. Result is a string of plain alphanumeric characters saved to a var

---
+++ PalindromeAssessor/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PalindromeAssessor
+{
+    public class Palindrome
+    {
+        public bool CheckIfPalindrome(string entry)
+        {
+            var cleanedEntry = Regex.Replace(entry, @"\W", string.Empty).ToLower();
+
+    }
+}
